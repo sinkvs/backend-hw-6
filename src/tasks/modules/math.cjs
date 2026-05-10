1fd8e4f321eaf154198ad01d9fe6f9a5d12e981b
Vladislav <sinkevichvs@vk.com>
2026-05-10 21:31:35 +0600

    2.1. CommonJS vs ESM: рабочие демо-файлы

---
+++ src/tasks/modules/math.cjs
@@ -0,0 +1,9 @@
+// Сложение
+function add(a, b) { return a + b; }
+// Вычитание
+function subtract(a, b) { return a - b; }
+// Умножение
+function multiply(a, b) { return a * b; }
+
+// Экспорт для CommonJS
+module.exports = { add, subtract, multiply };
